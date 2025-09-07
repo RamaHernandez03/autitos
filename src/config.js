ba0ab156508ba7578cf5b33d5a49723441ebe469
ramagammer <skwgrama@gmail.com>
2025-09-07 02:09:47 -0300

    New config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const config = {
   API_BASE_URL:
     process.env.REACT_APP_API_BASE_URL // ← si la seteás en Vercel, toma esa
     || (process.env.NODE_ENV === 'production'
-        ? 'autitos-production.up.railway.app' // ← backend
+        ? 'https://autitos-production.up.railway.app' // ← backend
         : 'http://localhost:8000'),
 };
 
